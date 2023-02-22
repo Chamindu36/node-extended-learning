3029c3557eb6565a70aaa8002d35b7a57ce88eeb
Chamindu <cudakara@gmail.com>
2023-02-23 00:23:34 +0530

    First draft of nasa-project  Node backend

---
+++ nasa-project/server/src/models/launches.model.js
@@ -0,0 +1,51 @@
+const launches = new Map();
+
+let latestFlightNumber = 100;
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler Exploration X",
+  rocket: "Expolrer IS1",
+  launchDate: new Date("2030-12-27"),
+  target: "Kepler-442 b",
+  customer: ["ZTM", "NASA"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber += 1;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      customers: ["ZTM", "NASA"],
+      upcoming: true,
+      success: true,
+    })
+  );
+}
+
+async function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+
+async function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+
+module.exports = {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+};
