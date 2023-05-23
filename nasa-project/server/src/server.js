523728d8bd94b7d43c8cd3eabedfd0cd81510f08
Chamindu <cudakara@gmail.com>
2023-05-24 01:00:42 +0530

    Add dotenv secret usage.

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,5 +1,7 @@
 const http = require("http");
 
+require("dotenv").config();
+
 const app = require("./app");
 const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
@@ -13,6 +15,7 @@ const server = http.createServer(app);
 async function startServer() {
   await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
 
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
